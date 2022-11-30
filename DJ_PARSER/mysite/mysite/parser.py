a2085dc6ef15a2aabc346ffcefd9e67bc47289e4
Alex <ax.dev@yahoo.com>
2022-12-01 01:00:12 +0300

    no gui ok, mypy + isort + settings

--- DJ_PARSER/mysite/mysite/parser.py
+++ DJ_PARSER/mysite/mysite/parser.py
@@ -1,6 +1,7 @@
 import requests
 from fake_useragent import UserAgent
 
+
 class Parser(object):
     def setSession(self):
         self.session = requests.session()
@@ -8,59 +9,60 @@ class Parser(object):
         ua = UserAgent()
         print(ua.chrome)
         # create header var in self oto store new fake useragent
-        self.fakeHeader = {'User-Agent':str(ua.chrome)}
+        self.fakeHeader = {"User-Agent": str(ua.chrome)}
         print(self.fakeHeader)
 
         self.session.headers.update(self.fakeHeader)
 
-    def setUrl(self,url):
-        if not isinstance(url,str) and (url < 10):
+    def setUrl(self, url):
+        if not isinstance(url, str) and (url < 10):
             return None
         self.url = url
 
     def doRequest(self):
         try:
             if not self.url:
-                print('not nonzero string is url -> ', self.url)
+                print("not nonzero string is url -> ", self.url)
             self.response = self.session.get(self.url)
             self.response.encoding = self.response.apparent_encoding
-            print('======request=====')
-            print('url:',self.url)
-            print('headers:',self.session.headers)
-            print('status:',self.session.cookies)
+            print("======request=====")
+            print("url:", self.url)
+            print("headers:", self.session.headers)
+            print("status:", self.session.cookies)
         except Exception as e:
             raise e
             return None
-        print('===response=====')
-        print('headers:',self.response.headers)
-        print('status:',self.response.status_code)
+        print("===response=====")
+        print("headers:", self.response.headers)
+        print("status:", self.response.status_code)
 
     def parseUrl(self):
         import bs4
+
         # print(self.response.text)
-        self.bs=bs4.BeautifulSoup(self.response.text, "html.parser")
+        self.bs = bs4.BeautifulSoup(self.response.text, "html.parser")
 
     def getLinkA(self, classStr=None):
         if classStr is None:
-            atag=self.bs.find_all('a')
+            atag = self.bs.find_all("a")
             for a in atag:
                 yield a
         elif isinstance(classStr, str):
-            atag = self.bs.find_all('a', {"class": classStr})
+            atag = self.bs.find_all("a", {"class": classStr})
             for a in atag:
                 yield a
-            pass #  {"class": "tm-article-snippet__title-link"}
+            pass  #  {"class": "tm-article-snippet__title-link"}
 
     def initSelenium(self):
         from selenium import webdriver
         from selenium.webdriver.common.by import By
-        from selenium.webdriver.support.ui import WebDriverWait
         from selenium.webdriver.support import expected_conditions as EC
+        from selenium.webdriver.support.ui import WebDriverWait
 
         self.browser = webdriver.Firefox()
         self.browser.get(self.url)
         self.browser.implicitly_wait(300)
-        elements = self.browser.find_elements(By.TAG_NAME, 'a')
+        elements = self.browser.find_elements(By.TAG_NAME, "a")
 
         for e in elements:
             print(e.text)
@@ -68,70 +70,58 @@ class Parser(object):
 
     def parseSeleniumUrl(self):
         import bs4
+
         # print(self.response.text)
-        self.bs=bs4.BeautifulSoup(self.seleniumHtml, "html.parser")
+        self.bs = bs4.BeautifulSoup(self.seleniumHtml, "html.parser")
 
     def runSelenium(self):
+        import time
+
         from selenium import webdriver
         from selenium.webdriver.chrome.service import Service
+        from selenium.webdriver.common.by import By
         from selenium.webdriver.support import expected_conditions as EC
         from selenium.webdriver.support.wait import WebDriverWait
-        from selenium.webdriver.common.by import By
-
-        import time
 
         ua = UserAgent()
         fua = ua.Chrome
-        url = 'https://mail.ru/'
-        #headers = {'User-Agent': fua} # Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
-
+        url = "https://mail.ru/"
+        # headers = {'User-Agent': fua} # Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36
 
         options = webdriver.ChromeOptions()
-        options.add_argument(f'user-agent={fua}') #"user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
+        options.add_argument(
+            f"user-agent={fua}"
+        )  # "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
         options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920x1080")
 
-        # driver = webdriver.Chrome(
-        #     executable_path="/path/chromedriver",
-        #     options=options
-        # )
+        s = Service(executable_path="/path/chromedriver")  # executable_path = './chromedriver'
 
-        s = Service(executable_path="/path/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options)
+        driver = webdriver.Chrome(service=s, options=options)  # Собираем драйвер
 
         try:
             driver.get(url)
-            #driver.get("https://www.vindecoderz.com/")
-            #driver.get(url)
+            driver.implicitly_wait(10)
 
-            # return html
-
-            # wait = WebDriverWait(driver, 0)
-            #element= wait.until(EC.presence_of_element_located((By.XPATH, '//a[@class="news-item-link"]')))
-            # print(element.text)
-            # time.sleep(0.2)
             # elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='news-item-link']")))
-            a_text_els =  driver.find_elements(By.CLASS_NAME, "news__list__item__link__text")
-            a_href_els =  driver.find_elements(By.CLASS_NAME, "news__list__item__link")
+            a_text_els = driver.find_elements(By.CLASS_NAME, "news__list__item__link__text")
+            a_href_els = driver.find_elements(By.CLASS_NAME, "news__list__item__link")
 
-            #a_elements =  driver.find_element_by_xpath("//span[@class=\"news__list__item__link\"]") #news__list__item__link news__list__item__link_simple
+            # a_elements =  driver.find_element_by_xpath("//span[@class=\"news__list__item__link\"]") #news__list__item__link news__list__item__link_simple
             # elems = driver.find_elements(By., "//*[@class='news__list__item__link__text']")
             data = dict()
-            for text, href in zip(a_text_els,a_href_els):
-                print("----a-------",len(a_text_els),len(a_href_els))
-                print(f'text :: {text.text}')
-                print(f"hreh ::     {href.get_attribute('href')}")
+            for text, href in zip(a_text_els, a_href_els):
+                print("----a-------", len(a_text_els), len(a_href_els))
+                print(f"text :: {text.text}")
+                print(f"hreh :: {href.get_attribute('href')}")
                 if not text.text:
-                    print('text out')
+                    print("text out")
                     break
-                data.update({'href':href.get_attribute('href')})
-                data.update({'text':text.text})
+                data.update({"href": href.get_attribute("href")})
+                data.update({"text": text.text})
                 yield data
-
-
-            # self.seleniumHtml = ' '.join(a_elements)
-            # print(a_elements)
-            # print(fua)
-            # print(self.seleniumHtml)
         except Exception as ex:
             print(ex)
         finally:
--- DJ_PARSER/mysite/mysite/views.py
+++ DJ_PARSER/mysite/mysite/views.py
@@ -4,13 +4,13 @@ from django.template import loader
 from mysite.parser import Parser
 
 def index(request):
-
     p = Parser()
     p.setUrl("https://habr.com/ru/all/")
     p.setSession()
     p.doRequest()
     p.parseUrl()
     latest_question_list = []#['habr', 'mail.ru','yandex']# paser.get()
+
 
     for a in p.getLinkA("tm-article-snippet__title-link"):
         # if len(a)>10:
@@ -21,8 +21,6 @@ def index(request):
             }
             )
 
-
-    #latest_question_list = []#['habr', 'mail.ru','yandex']# paser.get()
 
     m = Parser()
     m.setUrl("https://mail.ru/")
@@ -42,13 +40,7 @@ def index(request):
             }
         )
 
-    # for a in m.initSelenium():
-    #     # if len(a)>10:
-    #     mail_response_list.append(
-    #         {   "text": a.text,
-    #             "url":"https://mail.ru"+a['href']
-    #         }
-    #     )
+
     template = loader.get_template('index.html')
     context = {
         'latest_question_list': latest_question_list,
