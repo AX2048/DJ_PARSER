0ed6a3eabbfa0b9ae2d47c5ace7ff17721929ddf
Alex <ax.dev@yahoo.com>
2023-01-21 02:00:49 +0300

    GIT EX 3

--- LinuxEDU/dnsscr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "BEGIN"
-date +%F::%H-%M-%S
-pwd
-whoami
-echo "---------------"
-cat dns.txt | while read line; do echo $line; dig +short $line; echo "---------------"; done
-echo "END"
--- PHP/REG_FORM01/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "includes/db.php";
-
-unset($_SESSION['logged_user']);
-
-header('location: /');
-
-?>
